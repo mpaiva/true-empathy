d4d60b5bd8825aed807e2ecb831b40d48e19b867
Marcelo Paiva <mpaiva@gmail.com>
2024-07-26 18:05:09 -0400

    Refactor footer.php to remove commented out code and add TrueEmpathy.com - Creative Commons License

--- partials/footer.php
+++ partials/footer.php
@@ -1,8 +1,9 @@
     <footer class="footer-links">
-      <a href="#" id="footerAccessibilityStatement">Accessibility Statement</a>
-      <a href="#" id="footerTermsOfUse">Terms of Use</a>
+      <!-- <a href="#" id="footerAccessibilityStatement">Accessibility Statement</a>
+      <a href="#" id="footerTermsOfUse">Terms of Use</a> -->
+      TrueEmpathy.com - Creative Commons License
     </footer>
-    </div>
+
 
     <div class="mode-toggle text-xl" onclick="toggleMode()" aria-label="Toggle light/dark mode" id="toggleModeButton"><i
         class="fas fa-adjust"></i></div>
